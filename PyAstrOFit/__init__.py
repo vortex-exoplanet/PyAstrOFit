a20c4a81d9a3ab44f588dffb04d5fb70db1e3287
owertz <owertz@ulg.ac.be>
2015-07-28 11:21:35 +0200

    Logo update

--- PyAstrOFit/__init__.py
+++ PyAstrOFit/__init__.py
@@ -14,6 +14,21 @@ from .BayesianInference import *
 __version__ = "1.0.0"
 
 
+print "________                      _                                  ____    ________"
+print "`MMMMMMMb.                   dM.                                6MMMMb   `MMMMMMM 68b"         
+print " MM    `Mb                  ,MMb                /              8P    Y8   MM    \ Y89   /"     
+print " MM     MM ____    ___      d'YM.      ____    /M     ___  __ 6M      Mb  MM      ___  /M"     
+print " MM     MM `MM(    )M'     ,P `Mb     6MMMMb\ /MMMMM  `MM 6MM MM      MM  MM   ,  `MM /MMMMM"  
+print " MM    .M9  `Mb    d'      d'  YM.   MM'    `  MM      MM69   MM      MM  MMMMMM   MM  MM"     
+print " MMMMMMM9'   YM.  ,P      ,P   `Mb   YM.       MM      MM'    MM      MM  MM   `   MM  MM"     
+print " MM           MM  M       d'    YM.   YMMMMb   MM      MM     MM      MM  MM       MM  MM"     
+print " MM           `Mbd'      ,MMMMMMMMb       `Mb  MM      MM     YM      M9  MM       MM  MM"     
+print " MM            YMP       d'      YM. L    ,MM  YM.  ,  MM      8b    d8   MM       MM  YM.  ," 
+print "_MM_            M      _dM_     _dMM_MYMMMM9    YMMM9 _MM_      YMMMM9   _MM_     _MM_  YMMM9" 
+print "               d"                                                                             
+print "           (8),P"                                                                              
+print "            YMM"
+
 # TODO: checker aussi les packages comme psutil, ... mais simplement dire (par un warning) que s'il n'est pas installé
 # certaines méthodes ne fonctionneront pas.
 
